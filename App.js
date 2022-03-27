e427debd0112a8611c0999ea7bbe01a7b68aaab6
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-27 14:59:39 +0200

    Ajout HomeScreen and Navigation

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import SondagesScreen from './src/screens/SondagesScreen'
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerStyle: {
+          backgroundColor: "red"
+        },
+        headerTintColor: "white"
+      }}>
+
+        <Stack.Screen 
+          name='Home' 
+          component={HomeScreen}
+          options={{title: "Ecran d'accueil"}}
+          />
+
+        <Stack.Screen 
+          name='SondagesScreen' 
+          options={{title: "Sondages", }}
+          initialParams={{canalId: 1, currentUserId: 1}}
+          >
+            {(props)=> <SondagesScreen {...props}/>}
+        </Stack.Screen>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+
+function HomeScreen(props) {
+  const navigation = props.navigation;
+  
+  return (
+    <View style= {styles.container}>
+    <Text >Home Screen</Text>
+    <Button 
+      title='Go to Sondages' 
+      onPress={()=>navigation.navigate("SondagesScreen", {
+        canalId : 1,
+        currentUserId: 1
+      })}
+    />
+  </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default HomeScreen 
