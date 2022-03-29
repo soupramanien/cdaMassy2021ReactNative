f85f73d91968da14d700a1475c45c53c66e5879a
hwwrg <86141485+hwwrg@users.noreply.github.com>
2022-03-29 12:04:45 +0200

    Create Canaux.jsx

---
+++ src/components/Canal/Canaux.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import Canal from './Canal'
+
+function Canaux(props) {
+    return (
+        <View>
+            <Canal />
+        </View>
+    )
+}
