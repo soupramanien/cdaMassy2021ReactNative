79f2b2aab200dc5e0633fbabe3956d5666b40be4
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-29 12:53:26 +0200

    Correctif: json gitignore + ajout dependance package.json

--- src/screens/EFG/EFGAddScreen.js
+++ src/screens/EFG/EFGAddScreen.js
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import { ScrollView, Text, View, Button } from 'react-native';
-<<<<<<< HEAD
-import { Field, reduxForm } from 'redux-form';
 import EFGForm from '../../components/EFG/EFGForm';
-=======
->>>>>>> 243aa8b (add reliquat dispatch&add)
 
-=======
 import { StyleSheet, ScrollView, Text, View, Button } from 'react-native';
 import SelectingFormValuesForm from '../../components/EFG/EFGForm';
->>>>>>> 9d4eef4 (add preview for efg form)
 import { connect } from 'react-redux';
 import { Field, reduxForm, formValueSelector } from 'redux-form';
 
@@ -25,64 +18,7 @@ let EFGAddScreen = (props) => {
 	const [intituleEFG, setIntituleEFG] = useState('Nouvel exercice');
 
 	return (
-<<<<<<< HEAD
-		<ScrollView>
-			<Text>Création d'un exercice</Text>
-			<Text>Il y a {students} élèves dans le canal.</Text>
-<<<<<<< HEAD
-			<View>
-				<Field
-					name='intitule'
-					label="Intitulé de l'exercice"
-					component={EFGForm}
-				/>
-				<Field name='groupes' label='dede' component={EFGForm} value='sddde' />
-				<View>
-					<Field
-						name='membreParGroupe'
-						label="Membres d'élèves par groupes"
-						component={EFGForm.select}
-					/>
-				</View>
-				<View>
-					<Button
-						onPress={props.handleSubmit(onSubmit)}
-						buttonLabel='Submit'
-						title='Envoyer'
-					/>
-				</View>
-			</View>
-=======
 
-			<label>Intitulé de l'exercice</label>
-			<Field name='intitule' component='input' type='text' />
-
-			<label>Consignes</label>
-			<Field
-				name='consignes'
-				placeholder='Disponible dans la prochaine mise à jour'
-				component='input'
-				type='text'
-				disabled
-			/>
-
-			<label>Nombre d'élèves par groupe</label>
-			<Field name='studentsPerGroup' component='select'>
-				<option />
-				{students >= 4 && <option value='2'>2</option>}
-				{students >= 6 && <option value='3'>3</option>}
-				{students >= 8 && <option value='4'>4</option>}
-				{students >= 10 && <option value='5'>5</option>}
-			</Field>
-
-			<Button
-				onPress={props.handleSubmit(onSubmit)}
-				buttonLabel='Submit'
-				title='Envoyer'
-			/>
->>>>>>> 243aa8b (add reliquat dispatch&add)
-		</ScrollView>
-=======
 		<View style={styles.container}>
 			{console.log()}
 
@@ -115,7 +51,6 @@ let EFGAddScreen = (props) => {
 				</>
 			)}
 		</View>
->>>>>>> 9d4eef4 (add preview for efg form)
 	);
 };
 
