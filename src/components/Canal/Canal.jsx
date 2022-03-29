028eac49f494a98213e880ff2fc3aa0d9dc2da8b
hwwrg <86141485+hwwrg@users.noreply.github.com>
2022-03-29 12:04:42 +0200

    Create Canal.jsx

---
+++ src/components/Canal/Canal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Canal(props) {
+    return (
+        <View>
+            <Text>Canal 1</Text>
+            <Text>Canal 2</Text>
+            <Text>Canal 3</Text>
+        </View>
+    )
+}
