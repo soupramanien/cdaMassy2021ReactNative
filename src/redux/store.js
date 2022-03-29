79ebb8e59f032aeb5cff4ab9eda453bc58770a56
kamal9401 <kamalknina@hotmail.fr>
2022-03-29 17:42:06 +0200

    NGROK tunnel (voir description )
    pour utiliser   ngrok,  qui vous permet de creer un tunnel localhost  pour acceder à votre app depuis le mobile
    https://ngrok.com/download

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
-
+const URL_CONTEXT = 'http://291f-2a01-e0a-5db-3370-81bc-63aa-d6-a859.ngrok.io'; //tunnel kamal
+//const URL_CONTEXT = 'http://localhost:8080';
 const initialState = {
 	loading: false,
 	error: false,
 
 	utilisateur: {
-		idUtilisateurCourant: 2,
+		idUtilisateurCourant: 2
 	},
 
 	canal: {
@@ -16,8 +17,8 @@ const initialState = {
 			{ idCanal: 1, nom: 'CANAL N°1' },
 			{ idCanal: 2, nom: 'CANAL N°2' },
 			{ idCanal: 3, nom: 'CANAL N°3' },
-			{ idCanal: 4, nom: 'CANAL N°4' },
-		],
+			{ idCanal: 4, nom: 'CANAL N°4' }
+		]
 	},
 
 	question: {
@@ -34,8 +35,8 @@ const initialState = {
 						idProposition: 2,
 						idQuestion: 3,
 						libelle: 'protege',
-						estCorrecte: 1,
-					},
+						estCorrecte: 1
+					}
 				],
 				reponses: [
 					{
@@ -43,10 +44,10 @@ const initialState = {
 						idAuteur: 4,
 						nomAuteur: 'Marguerite Moulin',
 						libelle: 'ArrayList',
-						dateRendu: '2022-03-22 10:13:26',
-					},
+						dateRendu: '2022-03-22 10:13:26'
+					}
 				],
-				typeQuestion: 'LIBRE',
+				typeQuestion: 'LIBRE'
 			},
 			{
 				idQuestion: 2,
@@ -60,14 +61,14 @@ const initialState = {
 						idProposition: 1,
 						idQuestion: 2,
 						libelle: 'blanc',
-						estCorrecte: 1,
+						estCorrecte: 1
 					},
 					{
 						idProposition: 2,
 						idQuestion: 2,
 						libelle: 'gris',
-						estCorrecte: 1,
-					},
+						estCorrecte: 1
+					}
 				],
 				reponses: [
 					{
@@ -75,36 +76,36 @@ const initialState = {
 						idAuteur: 4,
 						nomAuteur: 'Marguerite Moulin',
 						libelle: 'ArrayList',
-						dateRendu: '2022-03-22 10:13:26',
-					},
+						dateRendu: '2022-03-22 10:13:26'
+					}
 				],
-				typeQuestion: 'LIBRE',
-			},
-		],
+				typeQuestion: 'LIBRE'
+			}
+		]
 	},
 	efgs: [
 		{
 			idEfg: 1,
 			createur: {
 				idCanal: 1,
-				idPersonne: 1,
+				idPersonne: 1
 			},
 			intitule: 'TP définir objectif',
 			groupes: '2,3',
 			idCanal: 1,
-			idCreateur: 1,
+			idCreateur: 1
 		},
 		{
 			idEfg: 2,
 			createur: {
 				idCanal: 1,
-				idPersonne: 2,
+				idPersonne: 2
 			},
 			intitule: 'TP définir but',
 			groupes: '2,2,3',
 			idCanal: 1,
-			idCreateur: 2,
-		},
+			idCreateur: 2
+		}
 	],
 	formateur: {
 		idPersonne: 0,
@@ -116,38 +117,35 @@ const initialState = {
 		est_formateur: 0,
 		est_gestionnaire: 0,
 		est_administrateur: 0,
-		allCanauxMembre: null,
-	},
+		allCanauxMembre: null
+	}
 };
 
 const actionTypes = {
 	LOAD_QUESTIONS: 'LOAD_QUESTIONS',
 	ASYNC_OP_START: 'ASYNC_OP_START',
 	ASYNC_OP_SUCCESS: 'ASYNC_OP_SUCCESS',
-	ASYNC_OP_FAILURE: 'ASYNC_OP_FAILURE',
+	ASYNC_OP_FAILURE: 'ASYNC_OP_FAILURE'
 };
 
 export const actionsCreators = {
 	setAsyncOperationStart: () => ({
-		type: actionTypes.ASYNC_OP_START,
+		type: actionTypes.ASYNC_OP_START
 	}),
 	setAsyncOperationSuccess: () => ({
-		type: actionTypes.ASYNC_OP_SUCCESS,
+		type: actionTypes.ASYNC_OP_SUCCESS
 	}),
 	setAsyncOperationFailure: () => ({
-		type: actionTypes.ASYNC_OP_FAILURE,
+		type: actionTypes.ASYNC_OP_FAILURE
 	}),
 	loadQuestions: (questions) => ({
 		type: actionTypes.LOAD_QUESTIONS,
-		value: questions,
+		value: questions
 	}),
 	loadQuestionsAsync: (idCanalSelectionne) => async (dispatch) => {
 		dispatch(actionsCreators.setAsyncOperationStart());
 		try {
-			const res = await fetch(
-				'http://localhost:8080/cdamassy2021/api/question/bycanal/' +
-					idCanalSelectionne
-			);
+			const res = await fetch(URL_CONTEXT + '/cdamassy2021/api/question/bycanal/' + idCanalSelectionne);
 			const newQuestions = await res.json();
 			dispatch(actionsCreators.loadQuestions(newQuestions));
 			dispatch(actionsCreators.setAsyncOperationSuccess());
@@ -156,10 +154,10 @@ export const actionsCreators = {
 			console.log(error);
 			dispatch(actionsCreators.setAsyncOperationFailure());
 		}
-	},
+	}
 };
 
-const reducers = function (state = initialState, action) {
+const reducers = function(state = initialState, action) {
 	switch (action.type) {
 		case actionTypes.ASYNC_OP_START:
 			return { ...state, loading: true };
@@ -170,7 +168,7 @@ const reducers = function (state = initialState, action) {
 		case actionTypes.LOAD_QUESTIONS:
 			return {
 				...state,
-				question: { ...state.question, questions: action.value },
+				question: { ...state.question, questions: action.value }
 			};
 		default:
 			return state;
