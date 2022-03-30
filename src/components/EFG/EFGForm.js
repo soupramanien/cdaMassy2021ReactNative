a82f26da38ad4a92e5892d9083b1fbc306cc0043
KheiraDaouadji <93580779+KheiraDaouadji@users.noreply.github.com>
2022-03-30 14:03:26 +0200

    'test pour le select '

--- src/components/EFG/EFGForm.js
+++ src/components/EFG/EFGForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Picker } from 'react-native';
+
 
 const EFGForm = (props) => {
+ 	const [selectedValue, setSelectedValue] = useState("membres par groupe");
 	// const { touched, error } = props.meta;
 	// const isErrorVisible = () => {
 	// 	return touched && error ? <Text>{error}</Text> : null;
@@ -10,10 +12,20 @@ const EFGForm = (props) => {
 		<View>
 			<Text>{props.label}</Text>
 			<View>
-				<TextInput />
+				<Picker
+				selectedValue={selectedValue}
+				onValueChange={(itemValue) => setSelectedValue(itemValue)}
+				>
+					<Picker.Item label="2" value="2"/>
+					<Picker.Item label="3" value="3"/>
+					<Picker.Item label="4" value="4"/>
+					<Picker.Item label="5" value="5"/>
+				</Picker>
+
 			</View>
 			{/* {isErrorVisible()} */}
 		</View>
 	);
 };
 export default EFGForm;
+// 
