47b5dae92e5b25c16b1e0dd7522f9d3611873587
Florian Langlois <florianlnglois@gmail.com>
2022-03-30 16:14:32 +0200

    Rebase EFGList ON EfgAdd

--- src/screens/EFG/EFGScreens.js
+++ src/screens/EFG/EFGScreens.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import { Text, View ,FlatList,TouchableOpacity} from 'react-native'
+import { useEffect, useState} from 'react';
+import { Text, View ,FlatList,TouchableOpacity,StyleSheet} from 'react-native'
 import EFGServices from '../../fetch/EFGfetch';
 
-function EFGScreens({navigation, route}) {
+function EFGScreens({navigation}) {
 
     const [canaux,setCanaux] = useState([
       {
@@ -20,18 +20,35 @@ function EFGScreens({navigation, route}) {
     },[]);
   
     return (
-      <View>
-          <Text>EFGScreens</Text>
+      <View style={styles.container}>
           <FlatList
               data={canaux}
               keyExtractor={(canal)=>String(canal.idCanal)}
               renderItem = {({item})=>(
-                  <TouchableOpacity>
-                      <Text>exercices du Canal {item.nom}</Text>
+                  <TouchableOpacity 
+                    style={styles.item}
+                    onPress={()=>navigation.navigate("EFGList",{
+                      idCanal : item.idCanal,
+                      nom : item.nom
+                    })}>
+                      <Text>Exercices du canal "{item.nom}"</Text>
                   </TouchableOpacity>
               )}
           />
       </View>
     )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  item : {
+    padding: 16,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray'
+  }
+});
 export default EFGScreens;
