f080f28cd0dce4f079984f50796347a6552be9a0
KheiraDaouadji <93580779+KheiraDaouadji@users.noreply.github.com>
2022-03-30 14:03:26 +0200

    'test pour le select '

--- src/components/EFG/EFGForm.js
+++ src/components/EFG/EFGForm.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-import { Text, View, TextInput, Picker} from 'react-native';
-import { Switch } from 'react-native-web';
-
-// Consigne 1 : Intégrer un switch "select" et "inputText"
-// Consigne 2 : Récupérer via console.log un JSON
-// Consigne 3 : (Utiliser les props) M'appeler quand t'as fini ça, parce que ça va être
-// chiant à gérer et j'ai pas encore trouvé comment faire.
-
-const EFGForm = (props) => {
-	const [selectedValue, setSelectedValue] = useState('membres par groupe');
-	// const { touched, error } = props.meta;
-	// const isErrorVisible = () => {
-	// 	return touched && error ? <Text>{error}</Text> : null;
-	// };
-	switch (form){
-		case 'intitule':
-			console.log('intitule');
-			break;
-		case'membreParGroupe':
-			console.log('membreParGroupe');
-			break;
-			
-		default:
-			break;
-
-	}
-
-
-	return (
-		<View>
-			<Text>{props.label}</Text>
-		
-					<Picker
-						selectedValue={selectedValue}
-						onValueChange={(itemValue) => setSelectedValue(itemValue)}>
-						<Picker.Item label='2' value='2' />
-						<Picker.Item label='3' value='3' />
-						<Picker.Item label='4' value='4' />
-						<Picker.Item label='5' value='5' />
-					</Picker>
-			
-			{/* {isErrorVisible()} */}
-		</View>
-	);
-};
-export default EFGForm;
-//
-=======
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { propTypes } from 'redux-form';
@@ -216,4 +166,3 @@ SelectingFormValuesForm = connect((state) => {
 })(SelectingFormValuesForm);
 
 export default SelectingFormValuesForm;
->>>>>>> 243aa8b (add reliquat dispatch&add)
