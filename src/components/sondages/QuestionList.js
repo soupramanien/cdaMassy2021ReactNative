6b8d480e3b0e351165bd127964d5bcf60dc608d6
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-31 11:14:20 +0200

    Recup: QuestionList.js

--- src/components/sondages/QuestionList.js
+++ src/components/sondages/QuestionList.js
@@ -1,47 +1,47 @@
 import Question from './Question';
 import { useEffect, useState, useCallback } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { actionsCreators } from '../../redux/store';
 
 function QuestionsList({ canalId }) {
-	//const [questions, setQuestions] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const idCanalSelectionne = useSelector((state) => state);
-
-	// Thomas, Kamal, Ben et Vinoth : c'est ici qu'on changé une ligne. On a juste rajouté ".reducer" après votre state pour que cela fonctionne. On passe donc à state.reducer.question.questions. Bisous, la teamVerte !
+	const dispatch = useDispatch();
+	// Changé ici aussi : state.reducer.loading
+	const loading = useSelector((state) => state.reducer.loading);
+	// Thomas, Kamal, Ben et Vinoth : c'est ici qu'on changé une ligne. On a juste rajouté ".reducer" après votre state pour que cela fonctionne. On passe donc à state.reducer.canal.idCanalSelectionne Bisous, la teamVerte !
+	const idCanalSelectionne = useSelector((state) => state.reducer.canal.idCanalSelectionne);
+	// Idem ici. state.reducer.question.questions
 	const questions = useSelector((state) => state.reducer.question.questions);
 
-	// const loadQuestions = useCallback(async () => {
-	//   setLoading(true);
-	//   try {
-	//     const res = await fetch("http://localhost:8080/cdamassy2021/api/question/bycanal/"+idCanalSelectionne);
-	//     const newQuestions = await res.json();
-	//     setQuestions(newQuestions);
-	//   } catch (error) {
-	//     alert("Network Error");
-	//   }
-	//   setLoading(false);
-	// }, [])
+	const loadQuestions = (idCanalSelectionne) => {
+		// Calls the thunk action creator, and passes the thunk function to dispatch
+		dispatch(actionsCreators.loadQuestionsAsync(idCanalSelectionne));
+	};
 
 	useEffect(() => {
-		//loadQuestions();
+		loadQuestions(idCanalSelectionne);
 	}, []);
 
 	return (
-		<FlatList
-			style={styles.questionList}
-			data={questions}
-			keyExtractor={(question) => String(question.idQuestion)}
-			contentContainerStyle={styles.container}
-			renderItem={({ item }) => (
-				<Question style={styles.item} question={item} />
-			)}
-			refreshing={loading}
-			// onRefresh={loadQuestions}
-		/>
+		<View>
+			{loading && <Text>loading...</Text>}
+
+			<FlatList
+				style={styles.questionList}
+				data={questions}
+				keyExtractor={(question) => String(question.idQuestion)}
+				contentContainerStyle={styles.container}
+				renderItem={({ item }) => <Question style={styles.item} question={item} />}
+				refreshing={loading}
+				onRefresh={loadQuestions}
+				showsVerticalScrollIndicator={true}
+				vertical
+			/>
+		</View>
 	);
 }
 export default QuestionsList;
+
 const styles = StyleSheet.create({
 	questionList: {
 		borderRadius: 12,
@@ -50,17 +50,12 @@ const styles = StyleSheet.create({
 		color: '#292929',
 		backgroundColor: '#dfecf7',
 		borderColor: '#4e4e4e',
-		flexDirection: 'column',
+		flexDirection: 'column'
 	},
-	container: {
-		flex: 1,
-	},
+	container: {},
 	item: {
-		padding: 20,
-		marginVertical: 8,
-		marginHorizontal: 16,
 	},
 	title: {
-		fontSize: 32,
-	},
+		fontSize: 32
+	}
 });
