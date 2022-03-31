a2028a0c97e09dd05c32b89a4a2e672df3c6cb96
HQRDa11 <thomasaserfroc@yahoo.fr>
2022-03-31 11:15:46 +0200

    Recup: CreerSondageScreen.js

--- src/screens/CreerSondageScreen.js
+++ src/screens/CreerSondageScreen.js
@@ -1,25 +1,29 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native'
+import QuestionForm from '../components/sondages/QuestionForm';
 
-function CreerSondageScreen({ navigation, route }) {
-	const { canalId, currentUserId } = route.params; //{itemId: 50, otherParam: "abc"}
 
-	return (
-		<View style={styles.container}>
-			<Text>Creer Sondage Screen</Text>
-			<Text>Selected canal : {canalId}</Text>
-			<Text>currentUserId: {currentUserId}</Text>
-			<Button title='Go Back' onPress={() => navigation.goBack()}>
-				Test
-			</Button>
-		</View>
-	);
+function CreerSondageScreen({navigation, route}) {
+    const {canalId, currentUserId} = route.params;//{itemId: 50, otherParam: "abc"}
+  
+  return (
+    <View style={styles.container}>
+        <Text>Creer Sondage Screen</Text>
+        <Text>Selected canal : {canalId}</Text>
+        <Text>currentUserId: {currentUserId}</Text>
+        <Button title='Go Back' onPress={()=>navigation.goBack()}/>
+
+        <QuestionForm canalId={canalId}></QuestionForm>
+
+        
+    </View>
+  )
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-export default CreerSondageScreen;
+export default CreerSondageScreen 
