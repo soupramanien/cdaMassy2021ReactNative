8aa21a20edfbb597603c9896fcee05cf2afbeab9
KheiraDaouadji <93580779+KheiraDaouadji@users.noreply.github.com>
2022-03-31 13:36:27 +0200

    ajoute du switch pour gérer le select et l'input text

--- src/components/EFG/EFGForm.js
+++ src/components/EFG/EFGForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Text, View, TextInput, Picker } from 'react-native';
+import { Text, View, TextInput, Picker} from 'react-native';
+import { Switch } from 'react-native-web';
 
 // Consigne 1 : Intégrer un switch "select" et "inputText"
 // Consigne 2 : Récupérer via console.log un JSON
@@ -12,19 +13,33 @@ const EFGForm = (props) => {
 	// const isErrorVisible = () => {
 	// 	return touched && error ? <Text>{error}</Text> : null;
 	// };
+	switch (form){
+		case 'intitule':
+			console.log('intitule');
+			break;
+		case'membreParGroupe':
+			console.log('membreParGroupe');
+			break;
+			
+		default:
+			break;
+
+	}
+
+
 	return (
 		<View>
 			<Text>{props.label}</Text>
-			<View>
-				<Picker
-					selectedValue={selectedValue}
-					onValueChange={(itemValue) => setSelectedValue(itemValue)}>
-					<Picker.Item label='2' value='2' />
-					<Picker.Item label='3' value='3' />
-					<Picker.Item label='4' value='4' />
-					<Picker.Item label='5' value='5' />
-				</Picker>
-			</View>
+		
+					<Picker
+						selectedValue={selectedValue}
+						onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+						<Picker.Item label='2' value='2' />
+						<Picker.Item label='3' value='3' />
+						<Picker.Item label='4' value='4' />
+						<Picker.Item label='5' value='5' />
+					</Picker>
+			
 			{/* {isErrorVisible()} */}
 		</View>
 	);
