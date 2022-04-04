d4e39b500214b87b93624f5541e98049b7be11ce
KheiraDaouadji <93580779+KheiraDaouadji@users.noreply.github.com>
2022-04-04 12:53:28 +0200

    EFG Form remplace

--- src/components/EFG/EFGForm.js
+++ src/components/EFG/EFGForm.js
@@ -1,48 +1,19 @@
-import { useState } from 'react';
-import { Text, View, TextInput, Picker} from 'react-native';
-import { Switch } from 'react-native-web';
-
-// Consigne 1 : Intégrer un switch "select" et "inputText"
-// Consigne 2 : Récupérer via console.log un JSON
-// Consigne 3 : (Utiliser les props) M'appeler quand t'as fini ça, parce que ça va être
-// chiant à gérer et j'ai pas encore trouvé comment faire.
+import React from 'react';
+import { Text, View, TextInput } from 'react-native';
 
 const EFGForm = (props) => {
-	const [selectedValue, setSelectedValue] = useState('membres par groupe');
 	// const { touched, error } = props.meta;
 	// const isErrorVisible = () => {
 	// 	return touched && error ? <Text>{error}</Text> : null;
 	// };
-	switch (form){
-		case 'intitule':
-			console.log('intitule');
-			break;
-		case'membreParGroupe':
-			console.log('membreParGroupe');
-			break;
-			
-		default:
-			break;
-
-	}
-
-
 	return (
 		<View>
 			<Text>{props.label}</Text>
-		
-					<Picker
-						selectedValue={selectedValue}
-						onValueChange={(itemValue) => setSelectedValue(itemValue)}>
-						<Picker.Item label='2' value='2' />
-						<Picker.Item label='3' value='3' />
-						<Picker.Item label='4' value='4' />
-						<Picker.Item label='5' value='5' />
-					</Picker>
-			
+			<View>
+				<TextInput />
+			</View>
 			{/* {isErrorVisible()} */}
 		</View>
 	);
 };
 export default EFGForm;
-//
