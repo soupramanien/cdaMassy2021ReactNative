f2afce86e91c3cd1e1947e49befc709a6b8ebcc0
hwwrg <86141485+hwwrg@users.noreply.github.com>
2022-04-04 12:14:59 +0200

    Revert "test merge"
    This reverts commit 87b5a5c03f44ab67fad6aadeab422858f495ce70.

--- src/components/Canal/Canaux.jsx
+++ src/components/Canal/Canaux.jsx
@@ -10,7 +10,14 @@ function Canaux(props){
     const canaux = useSelector(state => state.reducer.canal.canaux)
     const dispatch = useDispatch();
     const loadCanaux = async () =>{
-        console.log("Team Red")
+        try {
+            const res = await fetch("http://localhost:8080/cdamassy2021/api/canaux/" + idUtilisateurCourant )
+            const newCanaux = await res.json();
+            dispatch(actionsCreators.loadCanaux(newCanaux))
+        } catch (error) {
+            alert("Network Error")
+            console.log(error)
+        }
     };
 
     useEffect(() => {
